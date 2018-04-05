683dd004c1a374cb6931377e5ebdf47a6acd1cca
Joe M <34356804+jamot3@users.noreply.github.com>
2018-04-05 00:16:43 -0400

    Simplified into non-recursive method

--- selection_sort.rb
+++ selection_sort.rb
@@ -1,37 +1,24 @@
 ### I BELIEVE this is an implentation of selection sort. 
 
-def sort array 
-    recursive_sort array, []
-end
-
-def recursive_sort unsorted, sorted
-  
-    if unsorted.length == 0 
-        return sorted
-    end
-  
-    new_unsorted = []
-    x = 0 
-    current_smallest = unsorted.pop
-  
-    while x < unsorted.length
-        current_word = unsorted[x]
-    
-        if current_word < current_smallest 
-            new_unsorted.push(current_smallest)
-            current_smallest = current_word
-        else
-            new_unsorted.push(current_word)
+class Array
+    def sorter 
+        sorted = []
+        
+        while self.length > 0
+            index = 0
+            smallest = nil
+            while index < self.length
+                if smallest == nil || self[index] < smallest
+                    smallest = self[index]
+                end
+                index+=1
+            end
+            sorted.push(smallest)
+            self.delete(smallest)
         end
-    
-        x+=1
+        
+        sorted
     end
-  
-  
-    sorted.push(current_smallest)
-    unsorted = new_unsorted
-    recursive_sort unsorted, sorted
-  
 end
 
 puts sort(["monet", "ingres", "van gogh", "renoir", "degas", "cezanne", "mondrian"])
